b4044b4cb22d14faad0ea23b231d7a7336a8d175
Emmanuel Adesile <emma2adesile@gmail.com>
2019-11-20 20:47:15 +0100

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Emmanuel Adesile on 20/11/2019.
+//  Copyright © 2019 Emmanuel Adesile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
